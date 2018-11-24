280b9ae7e7a750505d1117bfcd5fd6ba9e56f0f2
Michael Lyon <lyoncodes@gmail.com>
2018-11-24 15:30:32 -0800

    I have no idea what is going on.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,65 @@
 'use strict'
-$(function () {
-  $.get('')// TODO
-})
-var Horns = function (image_url, title, description, keyword, horns) {
-  this.image_url = image_url
-  this.title = title
-  this.description = description
-  this.keyword = keyword
-  this.horns = horns
+
+function Horn (obj) {
+  this.image_url = obj.image_url
+  this.title = obj.title
+  this.description = obj.description
+  this.keyword = obj.keyword
+  this.horns = obj.horns
+
+  allHorns.push(this)
 }
-var firstHorn = new Horns()
+const allHorns = []
+const uniqueHorns = []
+console.log(allHorns)
+
+Horn.prototype.render = function () {
+  $('main').append('<div class = "entry"></div>')
+  let $entry = $('div[class = "entry"]')
+  let photoTemplate = $('#photo-template').html()
+  $entry.html(photoTemplate)
+
+  $entry.find('h2').text(this.title)
+  $entry.find('img').attr('src', this.image_url)
+  $entry.find('p').text(this.description)
+  $entry.find('h6').text(this.horns)
+  $entry.removeClass('entry')
+  $entry.attr('class', this.keyword)
+}
+Horn.prototype.menu = function () {
+  if (uniqueHorns.indexOf(this.keyword) === -1) {
+    $('select').append('<option class = "option"></option>')
+    let $option = $('option[class = "option"]')
+    $option.attr('value', this.keyword)
+    $option.text(this.keyword)
+    $option.removeClass('option')
+    uniqueHorns.push(this.keyword)
+  }
+}
+function readJson () {
+  $.get('./data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(hornObj => {
+        new Horn(hornObj)
+      })
+    })
+    .then(() => {
+      allHorns.forEach(horn => {
+        horn.render()
+        horn.menu()
+      })
+    })
+}
+$(() => readJson())
+console.log(allHorns)
+
+// $('select').on('change', function () {
+//   let $selection = $(this).val()
+//   if ($selection === 'default') {
+//     $('div').show()
+//     return
+//   }
+//   $('div').hide()
+//   $(`div[class = "${$selection}"]`).show()
+// })
+
